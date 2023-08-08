3abd5447314116e891eab0099ecefb21d87dc5ea
Daniel Langenkämper <dlangenk@gmail.com>
2023-08-08 09:57:36 +0200

    Update src/resources/assets/js/ol/MagicSamInteraction.js
    Co-authored-by: Martin Zurowietz <mzur@users.noreply.github.com>

--- src/resources/assets/js/ol/MagicSamInteraction.js
+++ src/resources/assets/js/ol/MagicSamInteraction.js
@@ -30,7 +30,7 @@ class MagicSamInteraction extends PointerInteraction {
             options.simplifyCount;
 
         this.map = options.map;
-        this.throttleInterval=options.throttleInterval;
+        this.throttleInterval = options.throttleInterval;
 
         this.sketchFeature = null;
         this.sketchSource = options.source;
