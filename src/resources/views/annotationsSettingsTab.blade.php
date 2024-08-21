9fea13ef558d4480915fa4efe007ef5447543cec
Leane Schlundt <lhecht@techfak.de>
2024-08-21 07:56:01 +0200

    Fix slider focus by using blur with onmouseup

--- src/resources/views/annotationsSettingsTab.blade.php
+++ src/resources/views/annotationsSettingsTab.blade.php
@@ -1,6 +1,6 @@
 <component :is="plugins.magicSam" :settings="settings" inline-template>
     <div class="sidebar-tab__section">
             <h5 title="Refresh rate for updating Magic SAM proposals">Magic SAM refresh rate (<span v-text="stepName"></span>)</h5>
-            <input type="range" min="0" max="4" step="1" v-model="stepIndex">
+            <input type="range" min="0" max="4" step="1" v-model="stepIndex" onmouseup="this.blur()">
     </div>
 </component>
